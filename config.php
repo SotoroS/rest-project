28b336a570572e69513652886ffe8be5fdcc0733
sotoros <sir.sotoros@ya.ru>
2020-01-29 16:23:07 +0400

    first commit

---
+++ config.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sotoros
+ * Date: 14.11.2019
+ * Time: 1:11
+ */
+
+return [
+	'id' => 'donate',
+	'basePath' => __DIR__,
+	'controllerNamespace' => 'micro\controllers',
+	'aliases' => [
+		'@micro' => __DIR__,
+	],
+	'defaultRoute' => 'site/index',
+	'components' => [
+		'urlManager' => [
+			'class' => 'yii\web\UrlManager',
+			'showScriptName' => false,
+			'enablePrettyUrl' => true,
+			'rules' => [
+				'<controller:\w+>/<id:\d+>' => '<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+				'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+			],
+		],
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=mailer',
+			'username' => 'root',
+			'password' => 'password',
+			
+		],
+		'request' => [
+			'enableCookieValidation' => false,
+			'enableCsrfValidation' => false,
+			'parsers' => [
+				'application/json' => 'yii\web\JsonParser',
+			]
+		],
+		'response' => [
+			'formatters' => [
+				\yii\web\Response::FORMAT_JSON => [
+					'class' => 'yii\web\JsonResponseFormatter',
+					'prettyPrint' => YII_DEBUG, // use "pretty" output in debug mode
+					'encodeOptions' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE,
+				],
+			],
+		],
+		'mailer' => [
+			'class' => 'yii\swiftmailer\Mailer',
+		],
+		'queue' => [
+			'class' => \yii\queue\db\Queue::class,
+			'db' => 'db', // DB connection component or its config
+			'tableName' => '{{%queue}}', // Table name
+			'channel' => 'default', // Queue channel key
+			'mutex' => \yii\mutex\MysqlMutex::class, // Mutex used to sync queries
+			'ttr' => 12960000,
+		],
+
+	],
+
+];
---
+++ console.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sotoros
+ * Date: 15.11.2019
+ * Time: 1:59
+ */
+
+return [
+	'id' => 'donate',
+	// basePath (базовый путь) приложения будет каталог `micro-app`
+	'basePath' => __DIR__,
+	// это пространство имен где приложение будет искать все контроллеры
+	'controllerNamespace' => 'micro\controllers',
+	// установим псевдоним '@micro', чтобы включить автозагрузку классов из пространства имен 'micro'
+	'aliases' => [
+		'@micro' => __DIR__,
+	],
+	'defaultRoute' => 'site/index',
+	'controllerMap' => [
+		'migrate' => [
+			'class' => 'yii\console\controllers\MigrateController',
+			'migrationPath' => null,
+			'migrationNamespaces' => [
+				'yii\queue\db\migrations',
+				'micro\migrations'
+			],
+		],
+	],
+	'components' => [
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=mailer',
+			'username' => 'root',
+			'password' => 'password',
+		],
+		'queue' => [
+			'class' => \yii\queue\db\Queue::class,
+			'db' => 'db', // DB connection component or its config
+			'tableName' => '{{%queue}}', // Table name
+			'channel' => 'default', // Queue channel key
+			'mutex' => \yii\mutex\MysqlMutex::class, // Mutex used to sync queries
+		],
+	],
+	'bootstrap' => [
+		'gii',
+		'queue',
+	],
+	'modules' => [
+		'gii' => [
+			'class' => 'yii\gii\Module',
+		],
+	],
+];
