56b2079ee387c9c1dfbd9cce311033e9049bad4e
Dyatko Gleb <glebikon@yandex.ru>
2020-02-01 03:03:57 +0400

    выплёвываем json url в actionGoogle

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -161,8 +161,7 @@ class UserController extends Controller
 
     public function actionGoogle()
     {
-        
-        //Step 1: Enter you google account credentials
+        //Enter you google account credentials
         $g_client = new Google_Client();
         
         $g_client->setClientId("156874812665-unh00vf96tmf4msn0j43fhie0b69k6ke.apps.googleusercontent.com");
@@ -170,47 +169,9 @@ class UserController extends Controller
         $g_client->setRedirectUri("http://rest.fokin-team.ru/user/google");
         $g_client->setScopes("email");
         
-        //Step 2 : Create the url
+        //Create the url
         $auth_url = $g_client->createAuthUrl();
-        echo '<script>location.replace("'.$auth_url.'")</script>';
-
-        //Step 3 : Get the authorization  code
-        $code = isset($_GET['code']) ? $_GET['code'] : NULL;
-
-        //Step 4: Get access token
-        if(isset($code)) 
-        {
-            try 
-            {
-        
-                $token = $g_client->fetchAccessTokenWithAuthCode($code);
-                $g_client->setAccessToken($token);
-        
-            }
-            catch (Exception $e)
-            {
-                echo $e->getMessage();
-            }
-        
-            try 
-            {
-                $pay_load = $g_client->verifyIdToken();
-        
-        
-            }
-            catch (Exception $e) 
-            {
-                echo $e->getMessage();
-            }
-        } 
-        else
-        {
-            $pay_load = null;
-        }
-        
-        if(isset($pay_load)){}
-        return $pay_load['email'];
-        
+        echo json_encode($auth_url);
     }
     public function actionCallBackGoogle()
     {
