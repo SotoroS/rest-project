5f0b8907688850df0acd7b397a385b9818d57789
Dyatko Gleb <glebikon@yandex.ru>
2020-02-01 00:46:22 +0400

    action google

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -136,6 +136,52 @@ class UserController extends Controller
 
     public function actionGoogle()
     {
+        //require ("vendor/autoload.php");
+        //Step 1: Enter you google account credentials
         
+        $g_client = new Google_Client();
+        
+        $g_client->setClientId("156874812665-unh00vf96tmf4msn0j43fhie0b69k6ke.apps.googleusercontent.com");
+        $g_client->setClientSecret("0qepssGons1TcyctkXfW-IPO");
+        $g_client->setRedirectUri("http://rest.fokin-team.ru/login/google");
+        $g_client->setScopes("email");
+        
+        //Step 2 : Create the url
+        $auth_url = $g_client->createAuthUrl();
+        echo "<a href='$auth_url'>Login Through Google </a>";
+        
+        //Step 3 : Get the authorization  code
+        $code = isset($_GET['code']) ? $_GET['code'] : NULL;
+        
+        //Step 4: Get access token
+        if(isset($code)) {
+        
+            try {
+        
+                $token = $g_client->fetchAccessTokenWithAuthCode($code);
+                $g_client->setAccessToken($token);
+        
+            }catch (Exception $e){
+                echo $e->getMessage();
+            }
+        
+        
+        
+        
+            try {
+                $pay_load = $g_client->verifyIdToken();
+        
+        
+            }catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        
+        } else{
+            $pay_load = null;
+        }
+        
+        if(isset($pay_load)){
+        }
+        return $pay_load['email'];
     }
 }
