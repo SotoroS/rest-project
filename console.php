bbd6f0cef7cf430a354444a5badcbb48e42db36a
shukanov <namra2014@gmail.com>
2020-02-06 00:16:42 +0400

    edit username, password MySql

--- console.php
+++ console.php
@@ -30,8 +30,8 @@ return [
 		'db' => [
 			'class' => 'yii\db\Connection',
 			'dsn' => 'mysql:host=localhost;dbname=rest',
-			'username' => 'root',
-			'password' => '',
+			'username' => 'GodOfDB',
+			'password' => 'hard_pass!',
 		],
 		'queue' => [
 			'class' => \yii\queue\db\Queue::class,
